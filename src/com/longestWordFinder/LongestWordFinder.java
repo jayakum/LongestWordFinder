b82191ff888e1ccd6d6b45741cf530802859ca78
Kumari, Jaya <jaya.kumari@sabre.com>
2018-07-31 15:05:37 -0500

    Updated test to handle multiple longest words test case

--- src/com/longestWordFinder/LongestWordFinder.java
+++ src/com/longestWordFinder/LongestWordFinder.java
@@ -3,42 +3,43 @@ package com.longestWordFinder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class LongestWordFinder {
 
     private static Logger logger = LogManager.getLogger();
 
     /**
-     * Returns the longest word and it's length from the given input sentence string.
+     * Returns the longest words and longest word length from the given input sentence string.
      * If user does not provide any input or provides invalid input, it returns the appropriate message.
      * Assumption: Only letters (a-zA-Z) are considered valid
+     * If there are more than one word with maximum length, all words are returned separated by comma (,)
      *
      * @param input sentence passed as string
-     * @return longest word and it's length as String value
+     * @return longest words and longest word length as String value
      */
     String getLongestWordAndLength(String input) {
+        String result, longestWords;
         String[] words = input.trim().replaceAll("[^a-zA-Z ]", "").split("\\s+");
-        String result, longestWord = null;
-        int longestWordLength = 0;
 
-        if (input.length() == 0) {
+        int longestWordLength = Arrays.stream(words).map(String::length).max(Integer::compareTo).orElse(0);
+
+        if (input.isEmpty()) {
             result = "Empty Input String";
-        } else if (words[0].equals("")) {
+        } else if (longestWordLength == 0) {
             result = "Invalid Input";
         } else {
-            for (String word : words) {
-                int wordLength = word.length();
-                if (wordLength > longestWordLength) {
-                    longestWord = word;
-                    longestWordLength = wordLength;
-                }
-            }
-            result = "Longest Word - " + longestWord + " : Length - " + String.valueOf(longestWordLength);
+            List<String> longestWordList = Arrays.stream(words)
+                    .filter(word -> word.length() >= longestWordLength)
+                    .collect(Collectors.toList());
+            longestWords = longestWordList.toString().replaceAll("^.|.$", "");
+            result = "Longest Word - " + longestWords + " : Length - " + longestWordLength;
         }
         return result;
     }
-
 
     public static void main(String[] args) {
         logger.info("Please enter input sentence:");
